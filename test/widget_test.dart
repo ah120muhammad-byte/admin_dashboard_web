a78912c5a1feb3bd296cb6a94703c2c335d3bab1
ah120muhammad-byte <ah120muhammad@gmail.com>
2026-09-01 19:17:21 +0300

    Fix widget test import for AdminLoginScreen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:admin_dashboard_web/admin/screens/admin_login_screen.dart';
 import 'package:admin_dashboard_web/main.dart';
 
 void main() {
