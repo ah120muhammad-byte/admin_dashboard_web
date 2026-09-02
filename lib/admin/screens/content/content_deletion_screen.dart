da7a6bf51547ee358bf8e8ab9a9ed365991d28f4
ah120muhammad-byte <ah120muhammad@gmail.com>
2026-09-02 22:42:42 +0300

    Add content cleanup screen

---
+++ lib/admin/screens/content/content_deletion_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContentDeletionScreen extends StatelessWidget {
+  const ContentDeletionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Content Cleanup'));
+  }
+}
